e8587a9c2836c4600ebc5e6cc055d2b56733cfb3
domb <woon4910@gmail.com>
2022-07-12 15:41:43 +0900

    feature: add Model group
    User.swift
    Merchandise.swift

---
+++ KarrotMarketCloneCoding/KarrotMarketCloneCoding/Model/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  KarrotMarketCloneCoding
+//
+//  Created by EHDOMB on 2022/07/12.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: Int
+    let nickName: String
+    let profileImageUrl: URL?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case nickName = "nickName"
+        case profileImageUrl = "profileImage"
+    }
+}
